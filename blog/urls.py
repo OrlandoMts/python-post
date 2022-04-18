fc10755fdd1f853e48f102119b6c1390499feb37
OrlandoMts <dani_monts19@hotmail.com>
2022-04-18 11:07:12 -0500

    Cree el formulario para crear un post

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView
 # Con este nombre es llamado en el namespace de las urls de Core
 app_name = "blog"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     # Si lo dejo vacio es porque la ruta es 'localhost/blog/' ya que asi lo declare
     # en el archivo de urls de Core 
     path('', BlogListView.as_view(), name='home'),
+    path('create-post/', BlogCreateView.as_view(), name='create-post'),
 ]
